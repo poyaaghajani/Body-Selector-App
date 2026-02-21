3aebe58b74f00c14dc629f99a5cfb3e7035d5863
Poya Aghajani <poyakisom1381@gmail.com>
2026-02-21 13:09:32 +0330

    add models and constants

---
+++ lib/consts.dart
@@ -0,0 +1,32 @@
+import 'package:body_selector/models/region.dart';
+
+final List<Region> regions = [
+  Region(name: 'سر', x: 0.32, y: 0.00, w: 0.36, h: 0.10),
+  Region(name: 'گردن', x: 0.42, y: 0.10, w: 0.16, h: 0.05),
+
+  Region(name: 'قفسه سینه', x: 0.28, y: 0.15, w: 0.44, h: 0.14),
+  Region(name: 'شکم', x: 0.30, y: 0.29, w: 0.40, h: 0.14),
+  Region(name: 'لگن', x: 0.32, y: 0.43, w: 0.36, h: 0.08),
+
+  Region(name: 'شانه چپ', x: 0.20, y: 0.15, w: 0.12, h: 0.06),
+  Region(name: 'بازوی چپ', x: 0.14, y: 0.20, w: 0.14, h: 0.12),
+  Region(name: 'آرنج چپ', x: 0.14, y: 0.30, w: 0.14, h: 0.06),
+  Region(name: 'ساعد چپ', x: 0.10, y: 0.36, w: 0.14, h: 0.10),
+  Region(name: 'دست چپ', x: 0.06, y: 0.46, w: 0.14, h: 0.10),
+
+  Region(name: 'شانه راست', x: 0.68, y: 0.15, w: 0.12, h: 0.06),
+  Region(name: 'بازوی راست', x: 0.72, y: 0.20, w: 0.14, h: 0.12),
+  Region(name: 'آرنج راست', x: 0.72, y: 0.30, w: 0.14, h: 0.06),
+  Region(name: 'ساعد راست', x: 0.74, y: 0.36, w: 0.14, h: 0.10),
+  Region(name: 'دست راست', x: 0.82, y: 0.46, w: 0.14, h: 0.10),
+
+  Region(name: 'ران چپ', x: 0.30, y: 0.48, w: 0.16, h: 0.18),
+  Region(name: 'زانوی چپ', x: 0.30, y: 0.64, w: 0.16, h: 0.07),
+  Region(name: 'ساق چپ', x: 0.28, y: 0.72, w: 0.16, h: 0.16),
+  Region(name: 'پای چپ', x: 0.26, y: 0.92, w: 0.18, h: 0.06),
+
+  Region(name: 'ران راست', x: 0.54, y: 0.48, w: 0.16, h: 0.18),
+  Region(name: 'زانوی راست', x: 0.54, y: 0.64, w: 0.16, h: 0.07),
+  Region(name: 'ساق راست', x: 0.56, y: 0.72, w: 0.16, h: 0.16),
+  Region(name: 'پای راست', x: 0.58, y: 0.92, w: 0.18, h: 0.06),
+];
---
+++ lib/models/region.dart
@@ -0,0 +1,12 @@
+class Region {
+  final String name;
+  final double x, y, w, h;
+
+  const Region({
+    required this.name,
+    required this.x,
+    required this.y,
+    required this.w,
+    required this.h,
+  });
+}
